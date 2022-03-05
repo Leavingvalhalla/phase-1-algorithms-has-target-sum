ca90ecc72cd8ad546ed8db6bff2f6cff85892a28
Zach Lefler <leaving.valhalla@gmail.com>
2022-03-05 10:57:50 -0800

    initial commit

--- index.js
+++ index.js
@@ -1,34 +1,52 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  if (array.length < 2) {
+    return false;
+  }
+  array.sort();
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        console.log(array[i], array[j]);
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  quadratic
 */
 
 /* 
-  Add your pseudocode here
+sort array
+start with first number, add second number
+stop when result is bigger than target
+move to second number, combine with third etc
 */
 
 /*
-  Add written explanation of your solution here
+Given an array of integers and a target integer, return true if any two
+numbers in the array can be added to equal the target integer. 
+Otherwise return false.  
+
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+  console.log('Expecting: true');
+  console.log('=>', hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+  console.log('');
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+  console.log('Expecting: true');
+  console.log('=>', hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+  console.log('');
 
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+  console.log('Expecting: false');
+  console.log('=>', hasTargetSum([1, 2, 5], 4));
 }
 
 module.exports = hasTargetSum;
